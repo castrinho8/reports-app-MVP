2a7063fea369442428bf48a51b113a909bc25b5f
Pablo Castro <castrinho8@gmail.com>
2015-08-07 20:04:38 +0200

    Add tabs and diferent components to ReportList
    Change-Id: I6c1e74e3a3c2a202e5f3e12a8eb538d9e2653601

---
+++ src/app/components/reportList/FinishedList.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import mui from 'material-ui';
+
+// Styles
+let centerListStyle = require('../../../assets/componentStyle/centerListStyle.js')
+
+// Components
+let ThemeManager = new mui.Styles.ThemeManager();
+let List = mui.List,
+   ListItem = mui.ListItem,
+   Avatar = mui.Avatar;
+
+let FinishedList = React.createClass( {
+
+   elements: [],
+
+   componentWillMount: function() {
+      this.elements = [
+         {local: 'Libertad', visitor: 'Greenpeace FC', localGoals: 1, visitorGoals: 2, localAvatarUrl:'assets/img/vacmatch.png', visitorAvatarUrl:'assets/img/vacmatch.png'},
+         {local: 'Castrillón', visitor: 'Somos Nós FC', localGoals: 0, visitorGoals: 4, localAvatarUrl:'assets/img/vacmatch.png', visitorAvatarUrl:'assets/img/vacmatch.png'},
+         {local: 'Bens FC', visitor: 'MoMa Bar', localGoals: 1, visitorGoals: 1, localAvatarUrl:'assets/img/vacmatch.png', visitorAvatarUrl:'assets/img/vacmatch.png'},
+         {local: 'Os codillos', visitor: 'Chiquitín', localGoals: 3, visitorGoals: 2, localAvatarUrl:'assets/img/vacmatch.png', visitorAvatarUrl:'assets/img/vacmatch.png'},
+         {local: 'Portagal', visitor: 'Caronium', localGoals: 2, visitorGoals: 2, localAvatarUrl:'assets/img/vacmatch.png', visitorAvatarUrl:'assets/img/vacmatch.png'},
+      ]
+   },
+
+  render() {
+
+      return (
+         <List style={centerListStyle}>
+            {
+               this.elements.map( match => {
+                  return <ListItem class="list-center"
+                     primaryText={ match.local + ' - ' + match.visitor }
+                     secondaryText={ match.localGoals + ' - ' + match.visitorGoals }
+                     leftAvatar={<Avatar src= { match.localAvatarUrl } />}
+                     rightAvatar={<Avatar src={ match.localAvatarUrl } />} />
+               })
+            }
+         </List>
+    )
+  }
+});
+
+module.exports = FinishedList
