92c8a4b274f7e9a76487e36aa9f72dfe1eab2bc7
Pablo Castro <castrinho8@gmail.com>
2015-08-09 22:37:54 +0200

    Add Report component
    Change-Id: Idc0fca6dd3101d16b20a2a1ccabbd1885f38e3a2

---
+++ src/app/components/report/reportStyle.js
@@ -0,0 +1,18 @@
+
+module.exports =
+{
+   buttonStyle : {
+      position:'relative',
+      opacity:'1',
+      fontSize:'14px',
+      letterSpacing:'0',
+      textTransform:'uppercase',
+      fontWeight:'500',
+      margin:'0',
+      padding:'0px 16px',
+      webkitUserSelect:'none',
+      lineHeight:'36px',
+      color:'rgba(0, 0, 0, 0.87)',
+      marginLeft: '20px'
+   }
+}
