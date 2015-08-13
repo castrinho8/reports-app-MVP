ba56644162c316692045208d0878ebcd913fcc92
Pablo Castro <castrinho8@gmail.com>
2015-08-13 18:48:32 +0200

    Add EndGame and SignReport components
    Change-Id: I5ee64d880479916359f96c9085748ee9afd94e4f

---
+++ src/app/components/endGame/endGameStyle.js
@@ -0,0 +1,13 @@
+module.exports =
+{
+   signButton: {
+      width: '40%',
+      marginLeft: '30%',
+      marginTop: '10%'
+   },
+
+   center: {
+      width: '60%',
+      marginLeft: '20%'
+   }
+}
